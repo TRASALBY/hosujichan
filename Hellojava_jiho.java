bdf4452c0738e8b585f7c4df05154f0f8d407d46
wvlgvh520 <72913381+wvlgvh520@users.noreply.github.com>
2020-10-18 14:52:20 +0900

    Update Hellojava_jiho.java

--- Hellojava_jiho.java
+++ Hellojava_jiho.java
@@ -1,5 +1,5 @@
-class  Test {  
-    public static void  main (String args []) {    
-     System.out.println ( "Hello Java_jiho" );  
-    }  
-}  
+class  Test {
+    public static void  main (String args []) {
+     System.out.println ( "정지호" );
+    }
+}
